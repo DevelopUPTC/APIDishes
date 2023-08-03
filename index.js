cf0022a9df92b00b218e4fe4bcf96706c1b0f7cc
Jairo Armando <jairo.armando@gmail.com>
2023-08-03 16:46:02 -0500

    Cambios para despliegue en vercel

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 const app = express()
 
 //ConnectDB
-require('../drivers/connect-db')
+require('./drivers/connect-db')
 
 //settings
 app.set('PORT',process.env.PORT || 3000 )
@@ -15,7 +15,7 @@ app.use(express.json())
 app.use(cors())
 
 //middleware
-app.use('/',require('../routes/index'))
+app.use('/',require('./routes/index'))
 
 app.listen(app.get('PORT'),()=>console.log(`Server Listen at Port ${app.get('PORT')}`))
 
